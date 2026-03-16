41af88c6a19a057adedae9d31fe54065c01d7518
José Rojas <josemiguebacco@gmail.com>
2026-03-16 10:09:15 +0100

    Interfaces

---
+++ src/main/java/com/equipo/soundbox/modelo/Puntuable.java
@@ -0,0 +1,4 @@
+public interface Puntuable {
+    void setPuntuacion(double puntuacion); // entre 0.0 y 10.0
+    double getPuntuacion();
+}
---
+++ src/main/java/com/equipo/soundbox/modelo/Reproducible.java
@@ -0,0 +1,7 @@
+package com.equipo.soundbox.modelo;
+
+public interface Reproducible {
+    void reproducir();      // método que cada álbum implementará a su manera
+    int getDuracionTotal(); // duración total en segundos
+}
+
